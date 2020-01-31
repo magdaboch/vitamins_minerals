501033a1cc5b69f1a244d008b228fe60161e0536
Magda <magda.bochinska@gmail.com>
2020-01-31 13:39:08 +0100

    zmiana na index.html

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -12,20 +13,20 @@ def home_page(request):
     return render(request, 'products_list.html', {"products": products})
 
 def search_products(request):
-    filter_value = ''
-    if request.method == 'GET':
-        # if request.GET['reset_filters'] == '1':
-        #     return redirect(to='products_list')
-        filter_value = request.GET['filter_products']
-        products = Product.objects.filter(Q(product_vitamin__vitamin_symbol=filter_value) |
-                                          Q(product_vitamin__vitamin_name=filter_value))\
-                                            .order_by('-valuevit__value')
-        products_mineral = Product.objects.filter(Q(product_mineral__mineral_name=filter_value)|
-                                                  Q(product_mineral__mineral_symbol=filter_value)) \
-                                                    .order_by('-valuemineral__value')
-        # raise Exception(products)
-    else:
-        products = Product.objects.all()
+    #     # if request.GET['reset_filters'] == '1':
+    #     #     return redirect(to='products_list')
+    filter_value = request.GET['filter_products'] if 'filter_products' in request.GET.keys() else ''
+    products = Product.objects.filter(Q(product_vitamin__vitamin_symbol=filter_value) |
+                                      Q(product_vitamin__vitamin_name=filter_value))\
+                                        .order_by('-valuevit__value')
+    products_mineral = Product.objects.filter(Q(product_mineral__mineral_name=filter_value)|
+                                              Q(product_mineral__mineral_symbol=filter_value)) \
+                                                .order_by('-valuemineral__value')
+    # raise Exception(products)
 
     return render(request, 'search_product.html', {"products":products, "products_mineral":products_mineral,
                                                    "filter_value":filter_value})
+
+@login_required
+def add_product(request):
+    pass
--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
 # Create your models here.
+class MyUser(User):
+    user_products = models.ManyToManyField(to=Product, null=True)
